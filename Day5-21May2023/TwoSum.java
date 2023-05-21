80be539b8306351c7a82603f2907d87d89af6c3f
developersview <pcslg1998@gmail.com>
2023-05-21 11:27:29 +0530

    for all possible solution of two sum

--- Day5-21May2023/TwoSum.java
+++ Day5-21May2023/TwoSum.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author pranoy.chakraborty
@@ -11,11 +13,12 @@ public class TwoSum {
                 findTwoSumElements(new int[]{3, 2, 4}, 6))
         );*/
         System.out.println(Arrays.toString(
-                findTwoSumIndex(new int[]{2,7,15,11},9))
+                findTwoSumIndex(new int[]{2, 7, 15, 11}, 9))
         );
         System.out.println(Arrays.toString(
-                findTwoSumElements(new int[]{2,7,15,11},9))
+                findTwoSumElements(new int[]{2, 7, 15, 11}, 9))
         );
+        System.out.println(findAllPossibleAnswers(new int[]{3, 2, 4, 5, 6, 7, 3, 3}, 6));
     }
 
     static int[] findTwoSumElements(int[] nums, int target) {
@@ -44,5 +47,18 @@ public class TwoSum {
             map.put(nums[i], i);
         }
         return null;
+    }
+
+    static List<List<Integer>> findAllPossibleAnswers(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                list.add(Arrays.asList(map.get(diff), i));
+            }
+            map.put(nums[i], i);
+        }
+        return list;
     }
 }
