9c2bceed487391878851caa0c5c91d1ed98f4c53
developersview <pcslg1998@gmail.com>
2023-06-04 00:27:11 +0530

    rotate array

---
+++ Day7-28May2023/RotateArray.java
@@ -0,0 +1,13 @@
+/**
+ * @author pranoy.chakraborty
+ * @Date 28/05/2023
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+
+    }
+
+    static int[] rotateArray(int[] arr,int k){
+       return null;
+    }
+}
