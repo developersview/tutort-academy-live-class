4094cf3ce500c28dc824b5882ab6d93dbe77b082
developersview <pcslg1998@gmail.com>
2023-06-04 10:52:54 +0530

    rotate image

---
+++ Day9-04June2023/RotateImage.java
@@ -0,0 +1,40 @@
+/**
+ * @author pranoy.chakraborty
+ * @Date 04/06/2023
+ */
+public class RotateImage {
+    public static void main(String[] args) {
+        rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+    }
+
+    static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            reverseArr(matrix[i]);
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void reverseArr(int[] arr) {
+        int i=0;
+        int j=arr.length-1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
