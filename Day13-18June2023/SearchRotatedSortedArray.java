80710f2a5010f9d645a0606723af093cddcd8fb3
developersview <pcslg1998@gmail.com>
2023-06-18 09:53:55 +0530

    search in a rotated sorted array

---
+++ Day13-18June2023/SearchRotatedSortedArray.java
@@ -0,0 +1,34 @@
+/**
+ * @author pranoy.chakraborty
+ * @Date 18/06/2023
+ */
+public class SearchRotatedSortedArray {
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+
+    static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target <= nums[high] && target >= nums[mid]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
